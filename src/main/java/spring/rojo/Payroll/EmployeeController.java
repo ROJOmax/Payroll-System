c8a9fc0f777b585bf0b376d38a7c395e26dfa878
YassBen <y.benkraouda@teletic.dz>
2025-12-15 16:53:28 +0100

    Refactor EmployeeController to RESTful HATEOAS-based API

--- src/main/java/spring/rojo/Payroll/EmployeeController.java
+++ src/main/java/spring/rojo/Payroll/EmployeeController.java
@@ -1,42 +1,117 @@
 package spring.rojo.Payroll;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 class EmployeeController {
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
     // tag::get-aggregate-root[]
-    @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
-    }
+//    @GetMapping("/employees")
+//    List<Employee> all() {
+//        return repository.findAll();
+//    }
     // end::get-aggregate-root[]
 
-    @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
+//    @GetMapping("/employees")
+//    CollectionModel<EntityModel<Employee>> all() {
+//
+//        List<EntityModel<Employee>> employees = repository.findAll().stream()
+//                .map(employee -> EntityModel.of(employee,
+//                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+//                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+//                .collect(Collectors.toList());
+//        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+//    }
+
+    @GetMapping("/employees")
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
-    // Single item
+
+//    // Single item
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+
+//    @GetMapping("/employees/{id}")
+//    EntityModel<Employee> one(@PathVariable Long id) {
+//        Employee employee = repository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//        return EntityModel.of(employee,
+//                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+//                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+//    }
+
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-
-        return repository.findById(id)
+    EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return assembler.toModel(employee);
     }
+
+
+//    @PostMapping("/employees")
+//    Employee newEmployee(@RequestBody Employee newEmployee) {
+//        return repository.save(newEmployee);
+//    }
+
+    @PostMapping("/employees")
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
+
+        EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
+
+//    @PutMapping("/employees/{id}")
+//    Employee replaceEmplyee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .map(employee -> {
+//                    employee.setName(newEmployee.getName());
+//                    employee.setRole(newEmployee.getRole());
+//                    return repository.save(employee);
+//                })
+//                .orElseGet(() -> {
+//                    return repository.save(newEmployee);
+//                });
+//    }
 
     @PutMapping("/employees/{id}")
-    Employee replaceEmplyee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+    ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee updatedEmployee = repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
@@ -45,11 +120,25 @@ class EmployeeController {
                 .orElseGet(() -> {
                     return repository.save(newEmployee);
                 });
+
+        EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
+//    @DeleteMapping("/employees/{id}")
+//    void deleteEmployee(@PathVariable Long id) {
+//        repository.deleteById(id);
+//    }
+
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/spring/rojo/Payroll/LoadDatabase.java
+++ src/main/java/spring/rojo/Payroll/LoadDatabase.java
@@ -15,8 +15,8 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(EmployeeRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Yassine Rojo", "Artist")));
+            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Yassine", "Rojo", "Artist")));
         };
     }
 
